7d50849636a4e04b76c836d9a27037d6ea221278
John Armstrong <j.armstrong484@gmail.com>
2016-04-18 18:52:56 +1200

    Moved all the route config to a serperate file named app.routes.js

--- www/js/app.js
+++ www/js/app.js
@@ -16,36 +16,6 @@ angular.module('listapp')
     });
   })
 
-  .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-    $stateProvider
-
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-      })
-      .state('app.lists', {
-        url: '/lists',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/lists.html',
-            controller: 'ListCtrl'
-          }
-        }
-      })
-
-      .state('app.items', {
-        url: '/lists/:listId',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/items.html',
-            controller: 'ItemsCtrl'
-          }
-        }
-      });
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/lists');
-
+  .config(function($httpProvider) {
     $httpProvider.interceptors.push('authInterceptor');
   });
