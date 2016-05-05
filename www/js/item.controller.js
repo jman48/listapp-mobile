ad413a99c2a690acc7905d1064051ed2ee397fc5
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-05 19:17:55 +1200

    Can now delete an item from the items menu

--- www/js/item.controller.js
+++ www/js/item.controller.js
@@ -6,12 +6,18 @@
       var listId = $stateParams.listId;
       $scope.items = [];
 
-      itemService.getItems(listId).then(function(items) {
-        $scope.items = items;
-      });
+      (function init() {
+        getItems();
+      })();
 
       $scope.showOptions = function(item) {
-        modalService.showItemOptions(item);
+        modalService.showItemOptions(item, getItems);
+      };
+
+      function getItems() {
+        itemService.getItems(listId).then(function(items) {
+          $scope.items = items;
+        });
       }
     })
 
