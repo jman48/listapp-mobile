9954e8b7e0f3e6841919a1062a1179f6813b9c10
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-05 19:14:53 +1200

    Added pop up options for items

--- www/js/modal.service.js
+++ www/js/modal.service.js
@@ -3,10 +3,11 @@
   angular.module('listapp.services')
     .service('modalService', modalService);
 
-  function modalService($ionicActionSheet, listService, popUpService, $state) {
+  function modalService($ionicActionSheet, listService, itemService, popUpService, $state) {
     var modalServ = {
-      showListOptions: showListOptions
-    }, hideActionSheet,
+        showListOptions: showListOptions,
+        showItemOptions: showItemOptions
+      }, hideActionSheet,
       hideCallBack;
 
     return modalServ;
@@ -20,14 +21,45 @@
         titleText: 'Modify your List',
         cancelText: 'Cancel',
         buttons: [
-          { text: 'Show'},
-          { text: 'Edit' }
+          {text: 'Show'},
+          {text: 'Edit'}
         ],
         cancel: function() {
           hideActionSheet();
         },
         destructiveButtonClicked: function() {
           listService.deleteList(list.id).then(function() {
+            hideOptions();
+          });
+        },
+        buttonClicked: function(index) {
+          if (index === 0) {
+            $state.go('app.items', {listId: list.id})
+          } else if (index == 1) {
+            popUpService.showListOptions(list, hideCallBack).then(hideActionSheet);
+          }
+          return true;
+        }
+      });
+    }
+
+    function showItemOptions(item, callBack) {
+      hideCallBack = callBack;
+
+      hideActionSheet = $ionicActionSheet.show({
+        buttons: [],
+        destructiveText: 'Delete',
+        titleText: 'Modify your Item',
+        cancelText: 'Cancel',
+        buttons: [
+          {text: 'Show'},
+          {text: 'Edit'}
+        ],
+        cancel: function() {
+          hideActionSheet();
+        },
+        destructiveButtonClicked: function() {
+          itemService.deleteItem(item).then(function() {
             hideOptions();
           });
         },
