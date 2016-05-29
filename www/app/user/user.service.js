8d9cad4119fff44ba3fd9096018d812915e7cabe
John Armstrong <j.armstrong484@gmail.com>
2016-05-29 20:00:23 +1200

    Added a user service and added a search function to it

---
+++ www/app/user/user.service.js
@@ -0,0 +1,24 @@
+(function() {
+
+  angular.module('listapp.services')
+    .service('userService', userService);
+
+  function userService($http, host, $q) {
+
+    var userServ = {
+      search: search
+    };
+
+    return userServ;
+
+    function search(search_string) {
+      var search = {search: {username: search_string}};
+
+      $http.post(host + '/users/search', search).then(function(response) {
+        return response.data;
+      }, function(response) {
+        $q.reject(response.data);
+      });
+    }
+  }
+})();
