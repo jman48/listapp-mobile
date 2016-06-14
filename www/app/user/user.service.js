72658cf178260d473f667d0d8bef370a86387f34
John Armstrong <john.armstrong@solnet.co.nz>
2016-06-15 09:44:31 +1200

    Will now cache results so that when user searches for a refined string from the original we can return results from local cache instrad of going to server

--- www/app/user/user.service.js
+++ www/app/user/user.service.js
@@ -1,9 +1,11 @@
-(function() {
+(function () {
 
   angular.module('listapp.services')
     .service('userService', userService);
 
   function userService($http, host, $q) {
+    var userCache = [],
+      cachedSearchString;
 
     var userServ = {
       search: search,
@@ -13,11 +15,28 @@
     return userServ;
 
     function search(search_string) {
-      var search = {search: {username: search_string}};
 
-      return $http.post(host + '/users/search', search).then(function(response) {
+      //Get users from local cache if we are only refining the list of users.
+      //e.g if we searched for "bob" then cache all results for bob so when we search for "bobb" we can get results from local cache
+      if (search_string.indexOf(cachedSearchString) >= 0) {
+        var users = [];
+
+        for (var i = 0; i < userCache.length; i++) {
+          if (userCache[i].username.indexOf(search_string) >= 0) {
+            users.push(userCache[i]);
+          }
+        }
+
+        return $q.resolve(users);
+      }
+
+      //If search string is not in cache then get results from server
+      cachedSearchString = search_string;
+
+      return $http.post(host + '/users/search', {search: {username: search_string}}).then(function (response) {
+        userCache = response.data;
         return response.data;
-      }, function(response) {
+      }, function (response) {
         $q.reject(response.data);
       });
     }
@@ -25,7 +44,7 @@
     function addUsers(list, users) {
       var addToList = {users: users};
 
-      return $http.post(host + '/lists/' + list.id + '/users', addToList).then(function(response) {
+      return $http.post(host + '/lists/' + list.id + '/users', addToList).then(function (response) {
         return response.data;
       });
     }
